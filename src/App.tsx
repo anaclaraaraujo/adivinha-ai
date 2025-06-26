a4e16049a4ff15b1349492fecd93149360391cab
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2025-06-26 20:25:42 -0300

    feat: add Letter and Tip components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import styles from './app.module.css'
 import { Header } from './components/Header'
+import { Letter } from './components/Letter';
+import { Tip } from './components/Tip';
 
 function App() {
 
@@ -11,6 +13,13 @@ function App() {
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Tip tip='Uma das linguagem de programação mais utilizadas' />
+        <div className={styles.words}>
+          <Letter value='J' />
+          <Letter value='A' />
+          <Letter value='V' />
+          <Letter value='' />
+        </div>
       </main>
     </div>
   )
