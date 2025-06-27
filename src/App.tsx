bdabbf51f47297b27fc3091923b0a6c6b06a99c2
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2025-06-27 16:03:39 -0300

    feat(game): validate guessed letter and update score

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,10 @@ import { WORDS, type Challenge } from "./utils/words"
 
 function App() {
   const [attempts, setAttempts] = useState(0)
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [letter, setLetter] = useState("")
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [letterUsed, setLetUsed] = useState<LettersUsedProps[]>([])
+  const [letterUsed, setLetterUsed] = useState<LettersUsedProps[]>([])
   const [challenge, setChallenge] = useState<Challenge | null>(null)
+  const [score, setScore] = useState(0)
 
   function handleRestartGame() {
     alert("Reinicie o jogo!");
@@ -27,6 +26,25 @@ function App() {
     setChallenge(randomWORD)
 
     setAttempts(0)
+    setLetter("")
+  }
+
+  function handleConfirm() {
+    if (!challenge) return
+    if (!letter.trim()) return alert("Digite uma letra!")
+
+    const value = letter.toUpperCase()
+    const exists = letterUsed.find((used) => used.value.toUpperCase() === value)
+
+    if (exists) return alert("Você já digitou essa letra " + value)
+
+    const hits = challenge.word.toUpperCase().split("").filter((chat) => chat === value).length
+
+    const correct = hits > 0
+    const currentScore = score + hits
+
+    setLetterUsed((prevState) => [...prevState, { value, correct }])
+    setScore(currentScore)
     setLetter("")
   }
 
@@ -42,17 +60,21 @@ function App() {
         <Header current={attempts} max={10} onRestart={handleRestartGame} />
         <Tip tip={challenge.tip} />
         <div className={styles.words}>
-          {
-            challenge.word.split("").map(() => (
-              <Letter />
-            ))}
+          {challenge.word.split("").map(() => (<Letter />))}
         </div>
+        
         <h4>Palpite</h4>
         <div className={styles.guess}>
-          <Input autoFocus maxLength={1} placeholder="?" />
-          <Button title="Confirmar" />
+          <Input
+            autoFocus
+            maxLength={1}
+            placeholder="?"
+            onChange={(e) => setLetter(e.target.value)}
+            value={letter}
+          />
+          <Button title="Confirmar" onClick={handleConfirm} />
         </div>
-        <LettersUsed data={letterUsed } />
+        <LettersUsed data={letterUsed} />
       </main>
     </div>
   )
