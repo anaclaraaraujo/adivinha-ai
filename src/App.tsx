fa22783278d7fa05c8bb5aaf1f45c03a45277446
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2025-06-27 15:26:28 -0300

    feat(game): implement word draw and display as letter boxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,58 @@
+import { useEffect, useState } from 'react';
 import styles from './app.module.css'
 import { Button } from './components/Button';
 import { Header } from './components/Header'
 import { Input } from './components/Input';
 import { Letter } from './components/Letter';
-import { LettersUsed } from './components/LettersUsed';
+import { LettersUsed, type LettersUsedProps } from './components/LettersUsed';
 import { Tip } from './components/Tip';
 
+import { WORDS, type Challenge } from "./utils/words"
+
 function App() {
+  const [attempts, setAttempts] = useState(0)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [letter, setLetter] = useState("")
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [letterUsed, setLetUsed] = useState<LettersUsedProps[]>([])
+  const [challenge, setChallenge] = useState<Challenge | null>(null)
 
   function handleRestartGame() {
-    alert("Reinicie");
+    alert("Reinicie o jogo!");
   }
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randomWORD = WORDS[index]
+    setChallenge(randomWORD)
+
+    setAttempts(0)
+    setLetter("")
+  }
+
+  useEffect(() => {
+    startGame()
+  }, [])
+
+  if (!challenge) return
 
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
-        <Tip tip='Uma das linguagem de programação mais utilizadas' />
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
+        <Tip tip={challenge.tip} />
         <div className={styles.words}>
-          <Letter value='J' />
-          <Letter value='A' />
-          <Letter value='V' />
-          <Letter value='' />
+          {
+            challenge.word.split("").map(() => (
+              <Letter />
+            ))}
         </div>
         <h4>Palpite</h4>
         <div className={styles.guess}>
           <Input autoFocus maxLength={1} placeholder="?" />
           <Button title="Confirmar" />
         </div>
-        <LettersUsed />
+        <LettersUsed data={letterUsed } />
       </main>
     </div>
   )
--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,13 +1,26 @@
 import { Letter } from "../Letter";
 import styles from "./styles.module.css"
 
-export function LettersUsed() {
+export type LettersUsedProps = {
+  value: string
+  correct: boolean
+}
+
+type Props = {
+  data: LettersUsedProps[]
+}
+
+export function LettersUsed({ data }: Props) {
   return (
     <div className={styles.lettersUsed}>
       <h5>Letras utilizadas</h5>
 
       <div>
-        <Letter value="X" />
+        {
+          data.map(({ value, correct }) => (
+            <Letter value={value} size="small" color={correct ? "correct" : "wrong"} />
+          ))
+        }
       </div>
     </div>
   );
